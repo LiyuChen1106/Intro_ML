be3e5194b5421809ee5e09f3ed551bfdb6fd9e46
Liyu Chen <esther.chen@mail.utoronto.ca>
2019-02-01 20:38:41 -0500

    draft1: logistic regression

--- a1/starter.py
+++ a1/starter.py
@@ -25,26 +25,41 @@ def loadData():
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
 def MSE(W, b, x, y, reg):
-    # Input x should be N(=3600) vectors
-    N, M= x.shape  # 3500x784
+    N, M= x.shape   # 3500x784
     mse = la.norm(np.dot(x, W) + b - y) ** 2
-    total_loss = 1 / (2 * N) * mse + reg / 2 * la.norm(W) ** 2
+    total_loss = 1 / (2 * N) * mse + reg / 2 * (la.norm(W) ** 2)
     return total_loss
 
 def gradMSE(W, b, x, y, reg):
-    # Your implementation here
-    N, M= x.shape
+    N, M= x.shape   # 3500x784
     grad_term = np.dot(x, W) + b - y
     gradMSE_bias = 1/N * np.sum(grad_term)
     gradMSE_W = 1/N * np.dot(x.T, grad_term) + reg * W
 
     return gradMSE_bias, gradMSE_W
 
-# def crossEntropyLoss(W, b, x, y, reg):
-    # Your implementation here
+def crossEntropyLoss(W, b, x, y, reg):
+    N, M = x.shape   # 3500x784
+    sigmoid = 1/(1 + np.exp(-np.dot(x, W) - b))  #sigmoid function with input Wx+b
+    cross_entropy = np.multiply(y, np.log(sigmoid)) + np.multiply(1-y, np.log(sigmoid))
+    total_loss = -1/N * np.sum(cross_entropy) + reg / 2 * (la.norm(W) ** 2)
+    return total_loss
 
-# def gradCE(W, b, x, y, reg):
-    # Your implementation here
+def gradCE(W, b, x, y, reg):
+    N, M = x.shape  # 3500x784
+    sigmoid = 1 / (1 + np.exp(-np.dot(x, W) - b))
+    gradCE_bias = -1/N * np.sum(y - sigmoid)
+    print("x dot W------------------------------")
+    print(np.dot(x,W))
+    print("y mult x------------------------------")
+    print(np.multiply(y, x))
+    print("sigmoid------------------------------")
+    print(sigmoid)
+    gradCE_W = -1/N * np.sum(np.multiply(y, x) - sigmoid) + reg * W
+    print("gradCE_W------------------------------")
+    print(gradCE_W)
+    return gradCE_bias, gradCE_W
+
 
 def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
     # Your implementation here
@@ -61,20 +76,29 @@ def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS
             break;
         train_bias = train_bias - alpha * gradMSE_bias
         mse = MSE(train_W, train_bias, train_dt_reshape, trainingLabels, reg)
+        print(i)
         print(mse)
         losses.append(mse)
+
+    plt.figure()
+    plt.plot(losses)
+    plt.show()
 
     return train_bias, train_W
 
 W = np.zeros((784,1))
 b = 0
 trainData, _,_,trainTarget,_,_ = loadData()
-train_bias, train_W = grad_descent(W,b,trainData,trainTarget,0.005,5000,0,1e-7)
+red = trainData.reshape(3500, -1)
+loss = crossEntropyLoss(W,b,red,trainTarget,0)
+# print(loss)
+gradCE(W,b,red,trainTarget,0)
+# print(gradMSE(W,b,red,trainTarget,0))
 
 print("-------------bias--------------")
-print(train_bias)
+# print(train_bias)
 print("-------------W-----------------")
-print(train_W)
+# print(train_W)
 
 
 # def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
