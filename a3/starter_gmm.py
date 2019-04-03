402bcc7ce5ae4de6f3f82b6be2cbeb556eac5875
Liyu Chen <esther.chen@mail.utoronto.ca>
2019-04-03 14:10:01 -0400

    A3 finished

--- a3/starter_gmm.py
+++ a3/starter_gmm.py
@@ -48,7 +48,7 @@ def log_posterior(log_PDF, log_pi):
     # Outputs
     # log_post: N X K
     numerator_log = tf.add(log_PDF, tf.transpose(log_pi))
-    denominator_log = -hlp.reduce_logsumexp(numerator_log, 1, True)
+    denominator_log = - hlp.reduce_logsumexp(numerator_log, 1, True)
     return tf.add(numerator_log, denominator_log)
 
 
@@ -81,14 +81,15 @@ def GMM(K, is_valid=False):
     phi = tf.Variable(tf.truncated_normal([K, 1], dtype=tf.float32))
     psi = tf.Variable(tf.truncated_normal([K, 1], dtype=tf.float32))  # tf.Variable(tf.ones([K, 1], dtype=tf.float32)/K)
     sigma_sq = tf.exp(phi)  # Kx1
-    log_pi = logsoftmax(psi)  # Kx1
+    log_pi = hlp.logsoftmax(psi)  # Kx1
 
     log_gauss = log_GaussPDF(X, MU, sigma_sq)  # NxK
-    log_post = log_posterior(log_gauss, log_pi)  # NxK
+    log_post = log_posterior(log_gauss, log_pi) #NxK
     assign_predict = tf.argmax(log_post, 1)
-    loss = - tf.reduce_sum(reduce_logsumexp(log_gauss + tf.transpose(log_pi), 1), axis=0)
+    loss = - tf.reduce_sum(hlp.reduce_logsumexp(log_gauss + tf.transpose(log_pi), 1), axis=0)
+    percentages = tf.unique_with_counts(assign_predict)
 
-    adam_op = tf.train.AdamOptimizer(learning_rate=0.01, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(loss)
+    adam_op = tf.train.AdamOptimizer(learning_rate=0.1, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(loss)
 
     init = tf.global_variables_initializer()
     sess = tf.InteractiveSession()
@@ -97,47 +98,73 @@ def GMM(K, is_valid=False):
     train_loss = []
     valid_loss = []
     for i in range(500):
-        _, MU_value, log_pi_values, variance, loss_value, log_post_value, predictions = sess.run(
-            [adam_op, MU, log_pi, sigma_sq, loss, log_post, assign_predict], feed_dict={X: train_data})
+        _, MU_value, log_pi_values, variance, loss_value, log_post_value, predictions, percentages_value = sess.run(
+            [adam_op, MU, log_pi, sigma_sq, loss, log_post, assign_predict, percentages], feed_dict={X: train_data})
         train_loss.append(loss_value)
 
         if is_valid:
             val_loss = sess.run(loss, feed_dict={X: val_data})
             valid_loss.append(val_loss)
 
-    #   print('final loss on train data :')
-    #   print(train_loss[len(train_loss) - 1])
-    # print('final loss on valid data :')
-    # print(valid_loss[len(valid_loss) - 1])
-    #   print(log_post_value)
-    #   print(predictions)
-
-    #   plt.figure(figsize=(6, 4))
-    #   plt.style.use('default')
-    #   # plt.subplot(121)
-    #   if is_valid:
-    #     plotLoss(valid_loss, K, is_valid=True)
-    #   else:
-    #     plotLoss(train_loss, K)
-
-    plt.figure(figsize=(6, 4))
-    plt.style.use('default')
-    plt.scatter(train_data[:, 0], train_data[:, 1], c=predictions, s=1, alpha=0.8)
-    plt.plot(MU_value[:, 0], MU_value[:, 1], 'r^', markersize=8)
-    plt.title('K = {}'.format(K))
+    # compute percentage
+    print(percentages_value)
+    sort_percentages = percentages_value[2][np.argsort(percentages_value[0])]
+    print(np.divide(sort_percentages, train_batch))
+    #   print(np.exp(log_pi_values[np.argsort(percentages_value[0])]))
+    #   print(variance[np.argsort(percentages_value[0])])
+    #   print(MU_value)
+    print('final loss on train data :')
+    print(train_loss[len(train_loss) - 1])
+    print('final loss on valid data :')
+    print(valid_loss[len(valid_loss) - 1])
 
 
-GMM(1)
-GMM(2)
-GMM(3)
-GMM(4)
-GMM(5)
+#   plt.figure(figsize=(6, 4))
+#   plt.style.use('default')
+#   plt.subplot(121)
+#   if is_valid:
+#     plotLoss(valid_loss, K, is_valid=True)
+#   else:
+#     plotLoss(train_loss, K)
+#   plt.savefig('gmm_loss_with_{}.png'.format(K))
 
+#   plt.figure(figsize=(6, 4))
+# #   plt.subplot(122)
+#   plt.style.use('default')
+#   plt.scatter(train_data[:, 0], train_data[:, 1], c=predictions, s=1, alpha=0.8)
+#   plt.plot(MU_value[:, 0], MU_value[:, 1], 'r^', markersize=8)
+#   plt.title('K = {}'.format(K))
+#   plt.savefig('gmm_loss_scatter_with_{}.png'.format(K))
+#   files.download('gmm_loss_scatter_with_{}.png'.format(K))
+
+#   plt.savefig('gmm_val_loss_scatter_with_{}.png'.format(K))
+#   files.download('gmm_val_loss_scatter_with_{}.png'.format(K))
+
+# GMM(1)
+# GMM(2)
+# GMM(3)
+# GMM(4)
 # GMM(5)
-# GMM(10)
-# GMM(15)
-# GMM(20)
-# GMM(30)
+
+# remember to correct the plot title!!!!!!!!!!!!
+# GMM(1, is_valid=True)
+# GMM(2, is_valid=True)
+# GMM(3, is_valid=True)
+# GMM(4, is_valid=True)
+# GMM(5, is_valid=True)
+
+# last part!!!!!!!!!!!!!
+# GMM(4, is_valid=True)
+GMM(5, is_valid=True)
+# # GMM(6, is_valid=True)
+# # GMM(7, is_valid=True)
+# # GMM(8, is_valid=True)
+GMM(9, is_valid=True)
+GMM(10, is_valid=True)
+GMM(11, is_valid=True)
+GMM(15, is_valid=True)
+GMM(20, is_valid=True)
+GMM(30, is_valid=True)
 
 plt.show()
 
--- a3/starter_kmeans.py
+++ a3/starter_kmeans.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import helper as hlp
 
 # Loading data
-# # data = np.load('data2D.npy')
+data = np.load('data2D.npy')
 # data = np.load('data100D.npy')
-# [num_pts, dim] = np.shape(data)
+[num_pts, dim] = np.shape(data)
 # # print(data)
 # # print(num_pts)
 # # print(dim)
@@ -20,9 +20,10 @@ def distanceFunc(X, MU):
   # pair_dist: is the pairwise distance matrix (NxK)
   expanded_X = tf.expand_dims(X, 0)
   expanded_MU = tf.expand_dims(MU, 1)
-  distances = tf.reduce_sum(tf.square(tf.subtract(expanded_X, expanded_MU)), 2) #KxN
+  distances = tf.reduce_sum(tf.square(tf.subtract(expanded_X, expanded_MU)), 2)  # KxN
 
   return distances
+
 
 def plotLoss(loss, K, is_valid=False):
   plt.plot(loss)
@@ -32,6 +33,7 @@ def plotLoss(loss, K, is_valid=False):
     plt.title('validation loss with K = {}'.format(K))
   else:
     plt.title('training loss with K = {}'.format(K))
+
 
 def kMeans(K, is_valid=False):
   print('Enter K = {} -----------------------------------------'.format(K))
@@ -47,15 +49,15 @@ def kMeans(K, is_valid=False):
     val_data = data[rnd_idx[:valid_batch]]
     train_data = data[rnd_idx[valid_batch:]]
 
-
   MU = tf.Variable(tf.random_normal([K, dim], dtype=tf.float32))
   X = tf.placeholder(tf.float32, [None, dim])
 
   distances = distanceFunc(X, MU)
   mins = tf.reduce_min(distances, 0)
   assign_indices = tf.argmin(distances, 0)
+  #   print(assign_indices.shape)
   loss_mu = tf.reduce_sum(mins, 0)
-  adam_op = tf.train.AdamOptimizer(learning_rate=0.01, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(loss_mu)
+  adam_op = tf.train.AdamOptimizer(learning_rate=0.1, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(loss_mu)
   percentages = tf.unique_with_counts(assign_indices)
 
   init = tf.global_variables_initializer()
@@ -65,37 +67,42 @@ def kMeans(K, is_valid=False):
   train_loss = []
   valid_loss = []
   for i in range(500):
-    _, MU_value, assignment_values, loss, percentages_value = sess.run([adam_op, MU, assign_indices, loss_mu, percentages], feed_dict={X: train_data})
+    _, MU_value, assignment_values, loss, percentages_value = sess.run(
+      [adam_op, MU, assign_indices, loss_mu, percentages], feed_dict={X: train_data})
     train_loss.append(loss)
     if is_valid:
       val_loss = sess.run(loss_mu, feed_dict={X: val_data})
       valid_loss.append(val_loss)
 
-
   # compute percentage
-  # print(np.divide(percentages_value[2:], train_batch))
-  # print('final loss on train data :')
-  # print(train_loss[len(train_loss) - 1])
-  # print('final loss on valid data :')
-  # print(valid_loss[len(valid_loss) - 1])
-  print(MU_value)
+  print(percentages_value)
+  sort_percentages = percentages_value[2][np.argsort(percentages_value[0])]
+  print(np.divide(sort_percentages, train_batch))
+  #   print(MU_value)
+  print('final loss on train data :')
+  print(train_loss[len(train_loss) - 1])
+  print('final loss on valid data :')
+  print(valid_loss[len(valid_loss) - 1])
 
 
-  # plt.figure(figsize=(16, 4))
-  # plt.subplot(121)
-  # if is_valid:
-  #   plotLoss(valid_loss, K, is_valid=True)
-  # else:
-  #   plotLoss(train_loss, K)
-  #
-  # plt.subplot(122)
-  plt.figure(figsize=(6, 4))
-  plt.scatter(train_data[:, 0], train_data[:, 1], c=assignment_values, s=1, alpha=0.5)
-  plt.plot(MU_value[:, 0], MU_value[:, 1], 'r^', markersize=8)
-  plt.title('K = {}'.format(K))
-  # plt.savefig('images/loss_scatter_with_{}.png'.format(K))
-  # plt.savefig('images/val_loss_scatter_with_{}.png'.format(K))
-  plt.savefig('images/Kmean_compare_scatter_with_{}.png'.format(K))
+#   plt.figure(figsize=(16, 4))
+#   plt.style.use('default')
+#   plt.subplot(121)
+#   if is_valid:
+#     plotLoss(valid_loss, K, is_valid=True)
+#   else:
+#     plotLoss(train_loss, K)
+
+#   plt.subplot(122)
+#   plt.scatter(train_data[:, 0], train_data[:, 1], c=assignment_values, s=1, alpha=0.8)
+#   plt.plot(MU_value[:, 0], MU_value[:, 1], 'r^', markersize=8)
+#   plt.title('K = {}'.format(K))
+#   plt.savefig('loss_scatter_with_{}.png'.format(K))
+#   files.download('loss_scatter_with_{}.png'.format(K))
+
+#   plt.savefig('val_loss_scatter_with_{}.png'.format(K))
+#   files.download('val_loss_scatter_with_{}.png'.format(K))
+#   plt.savefig('Kmean_compare_scatter_with_{}.png'.format(K))
 
 
 # kMeans(1)
@@ -112,9 +119,16 @@ def kMeans(K, is_valid=False):
 # kMeans(5, is_valid=True)
 
 # last part!!!!!!!!!!!!!
-kMeans(5)
-kMeans(10)
-kMeans(15)
-kMeans(20)
-kMeans(30)
+# kMeans(5)
+# kMeans(10)
+# kMeans(15)
+# kMeans(20)
+# kMeans(30)
+kMeans(4, is_valid=True)
+kMeans(5, is_valid=True)
+kMeans(6, is_valid=True)
+kMeans(10, is_valid=True)
+kMeans(15, is_valid=True)
+kMeans(20, is_valid=True)
+kMeans(30, is_valid=True)
 plt.show()
